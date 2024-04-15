d860a01b7bc0a07b660d27052a47a2cb1c904709
Pranav Kak <127792389+Pranavkak@users.noreply.github.com>
2024-04-15 21:19:58 +0530

    init

---
+++ con.py
@@ -0,0 +1,2 @@
+title = "Anshika's Dehazing and Desmoking Model for Indoors"
+description = f"You can use the model to dehaze hazy images. To quickly try out the model, you can choose from the available sample images below, or you can submit your own image. Not that, internally, the model is re-initialized based on the spatial dimensions of the input image and this process is time-consuming."
